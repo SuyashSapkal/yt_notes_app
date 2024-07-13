fa59556970b50ef6bb6cf1e5af59c50ebed2b0ee
Suyash Sapkal <suyash.sapkal@gmail.com>
2024-07-13 12:04:36 +0530

    feat: added notes and captions apis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,83 @@ const openai = new OpenAI({
     apiKey: OPEN_API_KEY
 });
 
-app.listen(PORT, ()=>{
+async function handleAINotes(text: string) {
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+                {
+                    role: 'system',
+                    content: 'you write bullet point notes:',
+                },
+                {
+                    role: 'user',
+                    content: text,
+                },
+            ],
+            // max_tokens: 64,
+            n: 1,
+        })
+
+        return response.choices[0].message.content
+    } catch (e) {
+        console.log(`there was an error while processing the notes from ai: ${e}`)
+
+        return '- Error while processing from the AI'
+    }
+}
+
+app.post('/notes', async (req, res) => {
+    const url = req.body.url
+    console.log(req.body.url)
+
+    if (!url) {
+        return res.status(400).json({
+            error: 'URL is required',
+        })
+    }
+
+    const msg = await YoutubeTranscript.fetchTranscript(url)
+    let text = ''
+    msg.forEach((item) => {
+        text += item.text + ' '
+    })
+    text = text.split('&amp;#39;').join("'")
+
+    let notes = null
+
+    try {
+        notes = await handleAINotes(text)
+    } catch (e) {
+        console.log(`there was an error while processing the notes: ${e}`)
+    }
+
+    return res.status(200).json({
+        notes: notes,
+    })
+})
+
+app.post('/captions', async (req, res) => {
+    const url = req.body.url
+
+    if (!url) {
+        return res.status(400).json({
+            error: 'URL is required',
+        })
+    }
+
+    const msg = await YoutubeTranscript.fetchTranscript(url)
+    let text = ''
+    msg.forEach((item) => {
+        text += item.text + ' '
+    })
+    text = text.split('&amp;#39;').join("'")
+
+    return res.status(200).json({
+        captions: text,
+    })
+})
+
+app.listen(PORT, () => {
     console.log(`backend server started on port ${PORT}`);
 });
